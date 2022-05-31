2cd9538ec08f4f1249320d95a7156cb28b8b12bf
Vignesh2208 <Vignesh2208@users.noreply.github.com>
2022-05-31 23:46:15 +0000

    Automated change: Fix sanity tests

--- src/core/ext/transport/chttp2/transport/context_list.h
+++ src/core/ext/transport/chttp2/transport/context_list.h
@@ -22,6 +22,7 @@
 #include <grpc/support/port_platform.h>
 
 #include <stddef.h>
+#include <stdint.h>
 
 #include "src/core/ext/transport/chttp2/transport/frame.h"
 #include "src/core/lib/iomgr/buffer_list.h"
